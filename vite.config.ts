0a9d2c8d6feb72ab6c6c82c1324c5f5052f99b50
LuanCKRG <luancamposck@gmail.com>
2025-03-19 16:49:51 -0300

    feat(vite): Add Tailwind CSS plugin to Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,15 @@
 import { defineConfig } from "vite"
 import path from "path"
 
+import tailwindcss from "@tailwindcss/vite"
+
 export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
     },
   },
+  plugins: [
+    tailwindcss()
+  ],
 })
