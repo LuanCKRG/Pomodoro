8f2bd8a20c529060a73c5a4583cc63f73c89fd32
LuanCKRG <luancamposck@gmail.com>
2025-03-19 18:09:25 -0300

    feat(dom): Add event listener for start timer button

--- src/dom/events.ts
+++ src/dom/events.ts
@@ -1,7 +1,10 @@
 import { handleCyclePhase } from "@/logic/cycle"
 import { CyclePhase } from "@/types"
-import { pomodoroButton, shortBreakButton, longBreakButton } from "./elements"
+import { pomodoroButton, shortBreakButton, longBreakButton, startTimerButton } from "@/dom/elements"
+import {startTimer}  from "@/logic/timer"
 
 pomodoroButton.addEventListener("click", () => handleCyclePhase(CyclePhase.pomodoro))
 shortBreakButton.addEventListener("click", () => handleCyclePhase(CyclePhase.shortBreak))
 longBreakButton.addEventListener("click", () => handleCyclePhase(CyclePhase.longBreak))
+
+startTimerButton.addEventListener("click", () => startTimer())
