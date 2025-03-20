0679b765137f5dee740715a1f507483d9af4f698
LuanCKRG <luancamposck@gmail.com>
2025-03-20 16:21:04 -0300

    feat(timer): Add updateActiveCycleButton function for cycle phase management

--- src/dom/ui.ts
+++ src/dom/ui.ts
@@ -1,4 +1,5 @@
-import { pauseTimerButton, startTimerButton } from "@/dom/elements"
+import { longBreakButton, pauseTimerButton, pomodoroButton, shortBreakButton, startTimerButton } from "@/dom/elements"
+import { CyclePhase } from "@/types"
 
 export function showStopTimerButton() {
   startTimerButton.classList.add("hidden")
@@ -8,4 +9,18 @@ export function showStopTimerButton() {
 export function showStartTimerButton() {
   startTimerButton.classList.remove("hidden")
   pauseTimerButton.classList.add("hidden")
+}
+
+export function updateActiveCycleButton(currentCycle: CyclePhase) {
+  pomodoroButton.classList.remove("active-cycle-button")
+  shortBreakButton.classList.remove("active-cycle-button")
+  longBreakButton.classList.remove("active-cycle-button")
+
+  if (currentCycle === CyclePhase.pomodoro) {
+    pomodoroButton.classList.add("active-cycle-button")
+  } else if (currentCycle === CyclePhase.shortBreak) {
+    shortBreakButton.classList.add("active-cycle-button")
+  } else if (currentCycle === CyclePhase.longBreak) {
+    longBreakButton.classList.add("active-cycle-button")
+  }
 }
