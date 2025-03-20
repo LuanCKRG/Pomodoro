78e3681b3127a84f0cef41afb7cd0e032088fe3d
LuanCKRG <luancamposck@gmail.com>
2025-03-20 15:45:58 -0300

    feat(timer): Add start and pause button functionality with UI updates for timer control

--- src/dom/events.ts
+++ src/dom/events.ts
@@ -1,10 +1,19 @@
 import { handleCyclePhase } from "@/logic/cycle"
 import { CyclePhase } from "@/types"
-import { pomodoroButton, shortBreakButton, longBreakButton, startTimerButton } from "@/dom/elements"
-import {startTimer}  from "@/logic/timer"
+import { pomodoroButton, shortBreakButton, longBreakButton, startTimerButton, pauseTimerButton } from "@/dom/elements"
+import {startTimer, stopTimer}  from "@/logic/timer"
+import { showStartTimerButton, showStopTimerButton } from "@/dom/ui"
 
 pomodoroButton.addEventListener("click", () => handleCyclePhase(CyclePhase.pomodoro))
 shortBreakButton.addEventListener("click", () => handleCyclePhase(CyclePhase.shortBreak))
 longBreakButton.addEventListener("click", () => handleCyclePhase(CyclePhase.longBreak))
 
-startTimerButton.addEventListener("click", () => startTimer())
+startTimerButton.addEventListener("click", () => {
+  startTimer()
+  showStopTimerButton()
+})
+
+pauseTimerButton.addEventListener("click", () => {
+  stopTimer()
+  showStartTimerButton()
+})
