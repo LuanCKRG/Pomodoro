9ff65b68114dae80a8a5cbfd59e3b8c39ad81c6d
LuanCKRG <luancamposck@gmail.com>
2025-03-21 16:56:30 -0300

    feat(events): Update button event listeners to handle theme changes on cycle phase selection

--- src/dom/events.ts
+++ src/dom/events.ts
@@ -3,10 +3,20 @@ import { CyclePhase } from "@/types"
 import { pomodoroButton, shortBreakButton, longBreakButton, startTimerButton, pauseTimerButton } from "@/dom/elements"
 import {startTimer, stopTimer}  from "@/logic/timer"
 import { showStartTimerButton, showStopTimerButton } from "@/dom/ui"
+import { handleTheme } from "@/dom/theme"
 
-pomodoroButton.addEventListener("click", () => handleCyclePhase(CyclePhase.pomodoro))
-shortBreakButton.addEventListener("click", () => handleCyclePhase(CyclePhase.shortBreak))
-longBreakButton.addEventListener("click", () => handleCyclePhase(CyclePhase.longBreak))
+pomodoroButton.addEventListener("click", () => {
+  handleCyclePhase(CyclePhase.pomodoro)
+  handleTheme("red")
+})
+shortBreakButton.addEventListener("click", () => {
+  handleCyclePhase(CyclePhase.shortBreak)
+  handleTheme("light-blue")
+})
+longBreakButton.addEventListener("click", () => {
+  handleCyclePhase(CyclePhase.longBreak)
+  handleTheme("dark-blue")
+})
 
 startTimerButton.addEventListener("click", () => {
   startTimer()
