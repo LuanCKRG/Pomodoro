45356d8b3d1d57de01e90524f45b91c4a4e3e6ce
LuanCKRG <luancamposck@gmail.com>
2025-03-22 23:22:01 -0300

    feat(theme): Update handleTheme function to use CyclePhase for dynamic theme changes

--- src/dom/theme.ts
+++ src/dom/theme.ts
@@ -1,7 +1,18 @@
 import { rootElement } from "@/dom/elements"
+import { CyclePhase } from "@/types"
 
-export function handleTheme(newTheme: "red" | "light-blue" | "dark-blue") {
+export function handleTheme(newPhase: CyclePhase) {
 	const currentTheme = rootElement.getAttribute("data-theme")
+
+	let newTheme = "red"
+
+	if (newPhase === CyclePhase.pomodoro) {
+		newTheme = "red"
+	} else if (newPhase === CyclePhase.shortBreak) {
+		newTheme = "light-blue"
+	} else if (newPhase === CyclePhase.longBreak) {
+		newTheme = "dark-blue"
+	}
 
 	if (currentTheme === newTheme) return
 
