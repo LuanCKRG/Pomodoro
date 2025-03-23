cb441b53256e4e8dac86595b2e3e80eedd754063
LuanCKRG <luancamposck@gmail.com>
2025-03-23 18:51:18 -0300

    refactor(events): Remove unused theme handler import

--- src/dom/events.ts
+++ src/dom/events.ts
@@ -1,5 +1,4 @@
 import { longBreakButton, pauseTimerButton, pomodoroButton, shortBreakButton, startTimerButton } from "@/dom/elements"
-import { handleTheme } from "@/dom/theme"
 import { showStartTimerButton, showStopTimerButton } from "@/dom/ui"
 import { handleCyclePhase } from "@/logic/cycle"
 import { startTimer, stopTimer } from "@/logic/timer"
