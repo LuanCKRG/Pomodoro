20a6738e7d59c05308c91d0dd140075245ec8fbf
LuanCKRG <luancamposck@gmail.com>
2025-03-27 20:02:28 -0300

    feat(modal): Add input fields for Pomodoro and break durations in config modal

--- src/dom/elements.ts
+++ src/dom/elements.ts
@@ -7,7 +7,13 @@ export const cycleButtons = {
 export const configModal = {
 	overlay: document.querySelector("#modal-overlay") as HTMLElement,
 	openButton: document.querySelector("#open-config-modal") as HTMLButtonElement,
-	closeButton: document.querySelector("#close-config-modal") as HTMLButtonElement
+	closeButton: document.querySelector("#close-config-modal") as HTMLButtonElement,
+
+	pomodoroValueInput: document.querySelector("#pom-input") as HTMLInputElement,
+	shortBreakvalueInput: document.querySelector("#short-input") as HTMLInputElement,
+	longBreakValueInput: document.querySelector("#long-input") as HTMLInputElement,
+
+	submitConfigButton: document.querySelector("#submit-config") as HTMLButtonElement
 }
 
 export const rootElement = document.documentElement as HTMLElement
