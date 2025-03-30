712c0ef48b6cbab114dc9671847cfb9df3bcb162
LuanCKRG <luancamposck@gmail.com>
2025-03-29 21:39:11 -0300

    feat(events): streamline event handling and update timer display on config submission

--- src/dom/events.ts
+++ src/dom/events.ts
@@ -2,37 +2,16 @@ import { configModal, cycleButtons, timerElements } from "@/dom/elements"
 import { setupIcons } from "@/dom/icons"
 import { getConfigInputValues, setupDefaultValuesOnInputs } from "@/dom/inputs"
 import { showPauseTimerButton, showStartTimerButton, toggleModal } from "@/dom/ui"
-import { handleCyclePhase } from "@/logic/cycle"
+import { handleCyclePhase, timer } from "@/logic/cycle"
 import { updatePhaseDurations } from "@/logic/phase-durations"
 import { startTimer, stopTimer } from "@/logic/timer"
 import { CyclePhase } from "@/types"
+import { updateTimerDisplay } from "@/utils/format"
 
 document.addEventListener("DOMContentLoaded", () => {
 	setupIcons()
 
 	setupDefaultValuesOnInputs()
-})
-
-cycleButtons.pomodoro.addEventListener("click", () => {
-	handleCyclePhase(CyclePhase.pomodoro)
-})
-cycleButtons.shortBreak.addEventListener("click", () => {
-	handleCyclePhase(CyclePhase.shortBreak)
-})
-cycleButtons.longBreak.addEventListener("click", () => {
-	handleCyclePhase(CyclePhase.longBreak)
-})
-
-timerElements.toggleButton.addEventListener("click", () => {
-	const currentState = timerElements.toggleButton.dataset.state
-
-	if (currentState === "pause") {
-		stopTimer()
-		showStartTimerButton()
-	} else if (currentState === "start") {
-		startTimer()
-		showPauseTimerButton()
-	}
 })
 
 // -------------* Modal *-------------
@@ -55,4 +34,6 @@ document.addEventListener("keydown", (e) => {
 configModal.submitConfigButton.addEventListener("click", () => {
 	const newPhaseDurations = getConfigInputValues()
 	updatePhaseDurations(newPhaseDurations)
+	updateTimerDisplay(timerElements.display, timer.minutes, timer.seconds)
+	toggleModal(false)
 })
